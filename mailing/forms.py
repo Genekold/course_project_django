deb2b184b9544c058c1bbea3cc0588a63a68c1c9
mihaylovea <mihailovevgenya@gmail.com>
2024-12-19 15:25:32 +0300

    Добавлен функционал работы с сообщениями (CRUD)

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,16 +1,22 @@
 from django.forms import ModelForm
 
-from mailing.models import MailingRecipient
+from mailing.models import MailingRecipient, Message
 
 
-class StyleFormBlog:
+class StyleForm:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
 
-class MailingRecipientForm(StyleFormBlog, ModelForm):
+class MailingRecipientForm(StyleForm, ModelForm):
     class Meta:
         model = MailingRecipient
+        fields = "__all__"
+
+
+class MessageForm(StyleForm, ModelForm):
+    class Meta:
+        model = Message
         fields = "__all__"
