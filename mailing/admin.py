3e9c881f12e1256699488bd9475d537740541e3c
mihaylovea <mihailovevgenya@gmail.com>
2024-12-21 17:46:31 +0300

    Добавлен функционал работы с рассылками(не полностью)

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from mailing.models import MailingRecipient, Message
+from mailing.models import MailingRecipient, Message, Mailing
 
 
 @admin.register(MailingRecipient)
 class MailingRecipientAdmin(admin.ModelAdmin):
     list_display = ('email', 'full_name', 'commentary')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'message')
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('start_date', 'end_date', 'status', 'message')
--- mailing/models.py
+++ mailing/models.py
@@ -21,7 +21,23 @@ class MailingRecipient(models.Model):
 
 
 class Message(models.Model):
-    """Класс объекта сообщение"""
+    """Класс объекта 'сообщение'"""
 
     subject = models.CharField(max_length=150, verbose_name="Тема сообщения", help_text="Тема")
     message = models.TextField(verbose_name="Тескт сообщения", help_text="Тескт сообщения")
+
+
+class Mailing(models.Model):
+    """Класс объекта 'рассылка'"""
+
+    STATUS_CHOICES = [("Создана", "Создана"), ("Запущена", "Запущена"), ("Завершена", "Завершена")]
+
+    start_date = models.DateTimeField(verbose_name="Дата и время первой рассылки")
+    end_date = models.DateTimeField(verbose_name="Дата и время окончания рассылки")
+    status = models.CharField(
+        max_length=9, verbose_name="Статус рассылки", blank=True, null=True, choices=STATUS_CHOICES, default="Создана"
+    )
+    message = models.ForeignKey(Message, verbose_name="сообщение", on_delete=models.CASCADE, related_name="mailings")
+    recipients = models.ManyToManyField(
+        MailingRecipient, verbose_name="Получатели рассылки", related_name="recipients"
+    )
