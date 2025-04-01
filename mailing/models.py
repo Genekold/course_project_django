42d88f4e5c2afc92317d01ac1300ec6e698c8db6
mihaylovea <mihailovevgenya@gmail.com>
2025-04-01 21:57:34 +0300

    CRUD закрыт группами разрешений (кроме функций представлений)

--- mailing/models.py
+++ mailing/models.py
@@ -12,6 +12,8 @@ class MailingRecipient(models.Model):
     )
     full_name = models.CharField(max_length=150, verbose_name="Ф.И.О. получателя")
     commentary = models.TextField(verbose_name="Комментарий о получателе рассылки", blank=True, null=True)
+    author = models.ForeignKey(get_user_model(), verbose_name='Автор рассылки', default=None, blank=True, null=True,
+                               related_name='recipients', on_delete=models.SET_NULL)
 
     def __str__(self):
         """Строковое представление получателя"""
--- mailing/views.py
+++ mailing/views.py
@@ -1,21 +1,29 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.transaction import commit
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 
 from mailing.forms import MailingRecipientForm, MessageForm, MailingForm
 from mailing.models import MailingRecipient, Message, Mailing, MailingAttempt
 from mailing.services import MailingService
 
 
-class MailingRecipientListView(ListView):
+class MailingRecipientListView(LoginRequiredMixin, ListView):
     """Класс представления списка получателей"""
     model = MailingRecipient
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.groups.filter(name="manager").exists():
+            return qs
+        return qs.filter(author=self.request.user)
 
-class MailingRecipientDetailView(DetailView):
+
+class MailingRecipientDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     """Класс детального представления одного получателя"""
     model = MailingRecipient
+    permission_required = 'mailing.view_mailingrecipient'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
@@ -25,28 +33,35 @@ class MailingRecipientDetailView(DetailView):
         return context
 
 
-
-class MailingRecipientCreateView(CreateView):
+class MailingRecipientCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Класс создания получателя"""
     model = MailingRecipient
     form_class = MailingRecipientForm
     success_url = reverse_lazy("mailing:recipient_list")
+    permission_required = 'mailing.add_mailingrecipient'
+
+    def form_valid(self, form):
+        recipient = form.save(commit=False)
+        recipient.author = self.request.user
+        return super().form_valid(form)
 
 
-class MailingRecipientUpdateView(UpdateView):
+class MailingRecipientUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """Класс изменения получателя"""
     model = MailingRecipient
     form_class = MailingRecipientForm
     success_url = reverse_lazy("mailing:recipient_list")
+    permission_required = 'mailing.change_mailingrecipient'
 
     def get_success_url(self):
         return reverse("mailing:recipient_detail", args=[self.kwargs.get('pk')])
 
 
-class MailingRecipientDeleteView(DeleteView):
+class MailingRecipientDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """Класс удаления получателя"""
     model = MailingRecipient
     success_url = reverse_lazy("mailing:recipient_list")
+    permission_required = 'mailing.delete_mailingrecipient'
 
 
 class MessageListView(ListView):
@@ -54,42 +69,59 @@ class MessageListView(ListView):
     model = Message
 
 
-class MessageDetailView(DetailView):
+class MessageDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     """Класс детального представления сообщения"""
     model = Message
+    permission_required = 'mailing.view_message'
 
 
-class MessageCreateView(CreateView):
+class MessageCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Класс создания сообщения"""
     model = Message
     form_class = MessageForm
     success_url = reverse_lazy("mailing:message_list")
+    permission_required = 'mailing.add_message'
+
+    def form_valid(self, form):
+        mailing = form.save(commit=False)
+        mailing.author = self.request.user
+        return super().form_valid(form)
 
 
-class MessageUpdateView(UpdateView):
+class MessageUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """Класс изменения сообщения"""
     model = Message
     form_class = MessageForm
     success_url = reverse_lazy("mailing:message_list")
+    permission_required = 'mailing.change_message'
 
     def get_success_url(self):
         return reverse("mailing:message_detail", args=[self.kwargs.get('pk')])
 
 
-class MessageDeleteView(DeleteView):
+class MessageDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """Класс удаления сообщения"""
     model = Message
     success_url = reverse_lazy("mailing:message_list")
+    permission_required = 'mailing.delete_message'
 
 
-class MailingListView(ListView):
+class MailingListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     """Класс представления рассылки"""
     model = Mailing
+    permission_required = 'mailing.view_mailing'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.groups.filter(name="manager").exists():
+            return qs
+        return qs.filter(author=self.request.user)
 
 
-class MailingDetailView(DetailView):
+class MailingDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     """Класс детального представления рассылки"""
     model = Mailing
+    permission_required = 'mailing.view_mailing'
 
     def get_context_data(self, **kwargs):
         mailing = Mailing.objects.get(pk=self.object.pk)
@@ -100,11 +132,12 @@ class MailingDetailView(DetailView):
         return context
 
 
-class MailingCreateView(CreateView):
+class MailingCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Класс создания рассылки"""
     model = Mailing
     form_class = MailingForm
     success_url = reverse_lazy("mailing:mailing_list")
+    permission_required = 'mailing.add_mailing'
 
     def form_valid(self, form):
         mailing = form.save(commit=False)
@@ -112,20 +145,22 @@ class MailingCreateView(CreateView):
         return super().form_valid(form)
 
 
-class MailingUpdateView(UpdateView):
+class MailingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """Класс изменения рассылки"""
     model = Mailing
     form_class = MailingForm
     success_url = reverse_lazy("mailing:mailing_list")
+    permission_required = 'mailing.change_mailing'
 
     def get_success_url(self):
         return reverse("mailing:mailing_detail", args=[self.kwargs.get('pk')])
 
 
-class MailingDeleteView(DeleteView):
+class MailingDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """Класс удаления рассылки"""
     model = Mailing
     success_url = reverse_lazy("mailing:mailing_list")
+    permission_required = 'mailing.delete_mailing'
 
 
 def index(request):
@@ -162,4 +197,3 @@ def statistic_mailing(request, mailing_id):
 def sehd_mail(request, mailing_id):
     MailingService.send_mail(mailing_id)
     return render(request, 'mailing/send_ok.html')
-
