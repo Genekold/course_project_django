114cd488f6635e6cb4eca9170414a7fe0eadfbc5
mihaylovea <mihailovevgenya@gmail.com>
2025-04-11 17:56:50 +0300

    Кэширование

--- mailing/models.py
+++ mailing/models.py
@@ -5,9 +5,7 @@ from django.db import models
 class MailingRecipient(models.Model):
     """Класс получателя рассылки"""
 
-    email = models.EmailField(
-        unique=True, verbose_name="email получателя рассылки"
-    )
+    email = models.EmailField(verbose_name="email получателя рассылки"    )
     full_name = models.CharField(max_length=150, verbose_name="Ф.И.О. получателя")
     commentary = models.TextField(verbose_name="Комментарий о получателе рассылки", blank=True, null=True)
     author = models.ForeignKey(get_user_model(), verbose_name='Автор рассылки', default=None, blank=True, null=True,
@@ -21,6 +19,9 @@ class MailingRecipient(models.Model):
         verbose_name = "Клиент"
         verbose_name_plural = "Клиенты"
         ordering = ["full_name"]
+        permissions = [
+            ('can_view_all_client', 'can view all client'),
+        ]
 
 
 class Message(models.Model):
@@ -64,6 +65,11 @@ class Mailing(models.Model):
         verbose_name = "Рассылка"
         verbose_name_plural = "Рассылки"
         ordering = ["status"]
+        permissions = [
+            ('can_view_statistic', 'can view statistic'),
+            ('can_view_all_mailing', 'can view all mailing'),
+            ('can_block_mailing', 'can block mailing'),
+        ]
 
 
 class MailingAttempt(models.Model):
--- mailing/services.py
+++ mailing/services.py
@@ -1,5 +1,6 @@
 import smtplib
 
+from django.core.cache import cache
 from django.core.mail import send_mail
 
 from config.settings import DEFAULT_FROM_EMAIL
@@ -32,12 +33,30 @@ class MailingService:
 
     @staticmethod
     def get_mailing():
-        return Mailing.objects.all()
+        """Получение всех рассылок для главной страницы"""
+
+        mailings_all = cache.get('mailings_all')
+        if not mailings_all:
+            mailings_all = Mailing.objects.all()
+            cache.set('mailings_all', mailings_all, 60 * 5)
+        return mailings_all
 
     @staticmethod
     def get_mailing_active():
-        return Mailing.objects.filter(status='Запущена')
+        """Получение рассылок со статусом 'Запущена' для главной страницы"""
+
+        mailings_active = cache.get('mailings_active')
+        if not mailings_active:
+            mailings_active = Mailing.objects.filter(status='Запущена')
+            cache.set('mailings_active', mailings_active, 60 * 5)
+        return mailings_active
 
     @staticmethod
     def get_recipient():
-        return MailingRecipient.objects.all()
+        """Получене всех уникальных получателей рассылки"""
+
+        client = cache.get('clients')
+        if not client:
+            client = MailingRecipient.objects.values('email').distinct()
+            cache.set('clients', client, 60 * 5)
+        return client
